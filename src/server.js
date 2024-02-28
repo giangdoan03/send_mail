0fdfa4dd7d118e77c0f9a20f84578f6575b5f244
Giang Doan <dvg8.vcc@gmail.com>
2024-02-28 11:36:16 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Server running');
   });
   app.listen(3000, () => {
     console.log('Server is listening on port 3000');
