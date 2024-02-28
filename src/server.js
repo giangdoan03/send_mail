c2254cd2d61643da0d2027e993931771b09c9fa9
Giang Doan <dvg8.vcc@gmail.com>
2024-02-28 13:16:15 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,31 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-    res.send('Server running');
-  });
-  app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
-  });
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  port: 465,
+  secure: true,
+  debug: true,
+  secureConnection: false,
+  auth: {
+    user: 'dvg8.vcc@gmail.com',
+    pass: 'vrjnrjoypjcebipu'
+  },
+  tls: {
+    rejectUnauthorized: true,
+  }
+});
+
+var mailOptions = {
+  from: 'doangiang665@gmail.com',
+  to: 'dvg8.vcc@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
